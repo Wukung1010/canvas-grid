787d52bc829c467437365b90e30c1d05c1b96658
sunxianfeng <sunxianfeng@jiuqi.com.cn>
2019-10-29 17:23:43 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpack = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'grid.bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpack({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ]
+}
